79d3b0c5edb718edd524bf49c828033ff30040d5
Leonardo Vilarinho <leonardo-i@outlook.com>
2017-04-27 19:50:17 -0300

    alterando server

--- server.php
+++ server.php
@@ -22,7 +22,9 @@ foreach ($_SERVER as $key => $value) {
 
 $conn = new mysqli($dbhost, $dbusername, $dbpassword, 'localdb');
 
-if (!$conn->connect_errno and isset($_POST['login']) and isset($_POST['senha']))) {
+mysqli_set_charset($conn, 'utf8');
+
+if ( !$conn->connect_errno and isset($_POST['login']) and isset($_POST['senha']) ) {
     $stmt = $obj_mysqli->prepare("INSERT INTO senhas (login, senha) VALUES (?,?)");
 
     $stmt->bind_param('ss', $_POST['login'], $_POST['senha']);
